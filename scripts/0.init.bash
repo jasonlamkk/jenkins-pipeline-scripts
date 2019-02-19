e0d56373b06cab2c4580b2d017e40d72ed5c6112
jasonlam <jlam@palo-it.com>
2019-02-19 15:47:01 +0800

    Fix known_host issue

--- scripts/0.init.bash
+++ scripts/0.init.bash
@@ -11,8 +11,14 @@ if [ $hasJq -eq 0 ]; then
     apk add jq
 fi
 
-echo "another example"
+echo "another example, prepare some folder"
 
 mkdir -p ${HOST_ROOT}/${BACKEND_FOLDER}
 
 mkdir -p ${HOST_ROOT}/${FRONTEND_FOLDER}
+
+echo "one more example, ensure the deploy target's ssh key is put into the known_host file"
+
+my_dir="$(dirname $0)"
+source "$my_dir/fetch-ip.bash"
+ssh-keyscan -t rsa ${DEPLOY_HOST} > ~/.ssh/known_hosts
